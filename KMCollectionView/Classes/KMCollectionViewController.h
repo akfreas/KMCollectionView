06e351be59e8d9de046ee1d2054bdbab53c07048
Alexander Freas <alex@komoot.de>
2015-02-25 12:51:26 +0100

    [collection view controller] Removed cancelation ID.

--- KMCollectionView/Classes/KMCollectionViewController.h
+++ KMCollectionView/Classes/KMCollectionViewController.h
@@ -1,7 +1,5 @@
 @interface KMCollectionViewController : UICollectionViewController
 
-@property (nonatomic) NSString *cancelationIdentifier;
-
 - (instancetype)initWithCollectionViewLayout:(UICollectionViewLayout *)layout;
 
 @end
