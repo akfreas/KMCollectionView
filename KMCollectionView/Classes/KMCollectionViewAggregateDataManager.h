7cf12b0066c84948d3eac5e3babf53e52c5c7cad
Matthias Friese <matthias@komoot.de>
2015-03-18 14:32:56 +0100

    remove deprecation declarations

--- KMCollectionView/Classes/KMCollectionViewAggregateDataManager.h
+++ KMCollectionView/Classes/KMCollectionViewAggregateDataManager.h
@@ -9,16 +9,8 @@
 - (void)linkDataSource:(KMCollectionViewDataSource *)dataSource;
 
 
-/* The following methods are deprecated in favor of the above method.
-   Instead of relying on a manually set section for a datasource to be 
-   associated with, the datasource will provide an instance of a datamanager.
-   We can infer the datasource by accessing the collection view's
-   datasource (which must be an aggregate source), and finally forwarding
-   the collection view delegate method off to the manager that is exposed
-   by that datasource.
-*/
-- (void)addDataManager:(KMCollectionViewDataManager *)dataManager forCorrespondingDataSource:(KMCollectionViewDataSource *)dataSource inGlobalSection:(NSInteger)section __deprecated;
-- (void)addDataManager:(KMCollectionViewDataManager *)dataManager forGlobalSection:(NSInteger)section __deprecated;
-- (void)associateDataSource:(KMCollectionViewDataSource *)dataSource withManagerInGlobalSection:(NSInteger)section __deprecated;
+- (void)addDataManager:(KMCollectionViewDataManager *)dataManager forCorrespondingDataSource:(KMCollectionViewDataSource *)dataSource inGlobalSection:(NSInteger)section;
+- (void)addDataManager:(KMCollectionViewDataManager *)dataManager forGlobalSection:(NSInteger)section;
+- (void)associateDataSource:(KMCollectionViewDataSource *)dataSource withManagerInGlobalSection:(NSInteger)section;
 
 @end
